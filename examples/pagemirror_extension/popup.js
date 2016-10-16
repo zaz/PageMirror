649e6c7b1f83520458d3f9c0b5a90ad7fc4f5be3
Zaz Brown <zazbrown@yandex.com>
2016-10-16 00:50:01 +0000

    WIP: popup

--- examples/pagemirror_extension/popup.js
+++ examples/pagemirror_extension/popup.js
@@ -1,15 +1,34 @@
 const server = "ws://127.0.0.1:1111"
 
-const to_tab = action => {
+let up   = JSON.parse( localStorage.getItem("up")   )
+let down = JSON.parse( localStorage.getItem("down") )
+let id   = JSON.parse( localStorage.getItem("id")   )
+let to   = JSON.parse( localStorage.getItem("to")   )
+
+current_tab = new Promise( (res) => {
 	chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
-		console.log("tab ID: " + tabs[0].id)
-		chrome.tabs.sendMessage(tabs[0].id, server);
-		url = chrome.extension.getURL('mirror.html?tabId=' + tabs[0].id)
+		res(tabs[0])
+	})
+})
+const toggle_up = () => { current_tab.then( tab => {
+		up = !up
+		localStorage.setItem("up", up)
+		if (!up) { return false }
+		chrome.tabs.sendMessage(tab.id, server);
+})}
+const toggle_down = () => { current_tab.then( tab => {
+		down = !down
+		localStorage.setItem("down", down)
+		if (!down) { return false }
+		url = chrome.extension.getURL('mirror.html?tabId=' + tab.id)
 		chrome.tabs.create({ url });
-	});
-}
+})}
 
 $( () => {
-	$("#up")  .click( () => { to_tab("up")   } )
-	$("#down").click( () => { to_tab("down") } )
+	if (up)
+		$("#up").attr("checked", true)
+	if (down)
+		$("#down").attr("checked", true)
+	$("#up")  .click( toggle_up )
+	$("#down").click( toggle_down )
 })
